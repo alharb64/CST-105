3f218ff4f9353a1e86bb185135e05510a18fa0a3
alharb64 <35380827+alharb64@users.noreply.github.com>
2018-01-14 12:09:38 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class Hello {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+      JOptionPane.showMessageDialog(null, "Hello World");
+   }
+}
